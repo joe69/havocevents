50256d90af67cc8e44c034e18520470a41d2c055
Thomas Entlicher <tentlicher@gmail.com>
2026-07-16 11:19:33 +0200

    Update

---
+++ theme_havoc/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Havoc Theme',
+    'description': 'Schwarz/Weiß Event-Theme für HAVOC — Ticketverkauf über Odoo Events.',
+    'summary': 'Black & white event theme with red accents for havoc.events',
+    'category': 'Theme/Entertainment',
+    'version': '19.0.1.0.0',
+    'author': 'wegot.vision',
+    'license': 'LGPL-3',
+    'depends': [
+        'website_event_sale',
+    ],
+    'assets': {
+        'web._assets_primary_variables': [
+            'theme_havoc/static/src/scss/primary_variables.scss',
+        ],
+        'web._assets_frontend_helpers': [
+            ('prepend', 'theme_havoc/static/src/scss/bootstrap_overridden.scss'),
+        ],
+        'web.assets_frontend': [
+            'theme_havoc/static/src/scss/theme.scss',
+        ],
+    },
+    'images': [
+        'static/description/cover.png',
+    ],
+}
